f1f6a4971192f23a81563e2515850a9ef15e7314
sdelphine <delph.sebast@gmail.com>
2019-12-12 12:20:40 +0100

    Delete a item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -23,6 +23,16 @@ const Mutations = {
             }
         },
         info)
+    },
+
+    async deleteItem(parent, args, ctx, info) {
+        const where = { id: args.id };
+        // find the item
+        const item = await ctx.db.query.item({ where }, `{ id title }`);
+        // Check if they own that item or have the permission
+        // TODO
+        //Delete it
+        return ctx.db.mutation.deleteItem({ where }, info)
     }
 };
 
--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -5,6 +5,7 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import Link from 'next/link';
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem'
 
 
 class Item extends Component {
@@ -35,7 +36,7 @@ class Item extends Component {
                         <a>Edit ✏ </a>
                     </Link>
                     <button>Add To Cart</button>
-                    <button>Delete</button>
+                    <DeleteItem id={item.id}>Delete this item</DeleteItem>
 
                 </div>
 
